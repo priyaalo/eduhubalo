b87b2a4eba36c3a1affc90312295d19f1ed9fc49
priyajira <priyanka@aloinfotech.in>
2025-10-06 15:27:19 +0530

    commit changes

--- src/sections/CoursesSection/studentsection/StudentSection.jsx
+++ src/sections/CoursesSection/studentsection/StudentSection.jsx
@@ -8,14 +8,14 @@ import colon from "@/assets/courses/colon.png";
 
 const StudentSection = () => {
   const icons = [
-    { id: 1, name: "Akram Shihaf A", text: "ALO Educational Hub sharpened my Graphic Design skills, giving me the creative edge to excel in the design industry", image: man2, image1: colon },
-    { id: 2, name: "kajitha", text: "Learning Digital Marketing at ALO Educational Hub sharpened my skills and prepared me to succeed in the dynamic marketing world.", image: man3, image1: colon },
+    { id: 1, name: "Aravind ", text: "ALO Educational Hub sharpened my Graphic Design skills, giving me the creative edge to excel in the design industry", image: man2, image1: colon },
+    { id: 2, name: "Abitha", text: "Learning Digital Marketing at ALO Educational Hub sharpened my skills and prepared me to succeed in the dynamic marketing world.", image: man3, image1: colon },
     { id: 3, name: "Abitha", text: "Thanks to ALO Educational Hub, I gained the knowledge and skills to confidently navigate the dynamic world of Digital Marketing.", image: man4, image1: colon },
-    { id: 4, name: "Vaishnavi", text: "ALO Educational Hub refined my Frontend Development abilities, giving me the confidence to deliver engaging and efficient web solutions.", image: man, image1: colon },
-    { id: 5, name: "Isaikumar", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.", image: man, image1: colon },
-    { id: 6, name: "Isaikumar", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.", image: man, image1: colon },
-    { id: 7, name: "Isaikumar", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.", image: man, image1: colon },
-    { id: 8, name: "Isaikumar", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.", image: man, image1: colon },
+    { id: 4, name: "Vaishnavi", text: "ALO Educational Hub refined my Frontend Development abilities, giving me the confidence to deliver web solutions.", image: man, image1: colon },
+    // { id: 5, name: "Isaikumar", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.", image: man, image1: colon },
+    // { id: 6, name: "Isaikumar", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.", image: man, image1: colon },
+    // { id: 7, name: "Isaikumar", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.", image: man, image1: colon },
+    // { id: 8, name: "Isaikumar", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.", image: man, image1: colon },
   ];
 
 
@@ -40,7 +40,7 @@ const StudentSection = () => {
               > <div className={styles.img1}>
                 <img src={item.image1.src} className={styles.colon} alt="colon" /></div>
                 <div className={styles.img2}>
-                <img src={item.image.src} alt={item.name} /></div>
+                <img src={item.image.src} className={styles.imagecard} alt={item.name} /></div>
                 <p>{item.text}</p>
                 <h5>{item.name}</h5>
               </div>
